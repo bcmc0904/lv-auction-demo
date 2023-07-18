7a32ef90f727dfb6a85beaeaaf8f1fb4fdcb7c65
Thai <bcmc0904@gmail.com>
2023-07-18 16:39:41 +0700

    Save

--- src/app/auction/[id]/page.tsx
+++ src/app/auction/[id]/page.tsx
@@ -15,6 +15,9 @@ export default function AuctionDetails({params}: {params: {id: string}}) {
   const [bidPrice, setBidPrice] = useState<number>(0)
   const [curTopPrice, setCurTopPrice] = useState<number>(0)
   const [curUserTopPrice, setCurUserTopPrice] = useState<number>(0)
+  const [userDenied, setUserDenied] = useState<boolean>(false)
+  const [userWon, setUserWon] = useState<boolean>(false)
+  const [userFinalizeBid, setUserFinalizeBid] = useState<boolean>(false)
   const {auctions, user, getAllAuctions} = useAuths()
   const getData = async () => {
     if (!auctions || !auctions.length) return
@@ -25,9 +28,15 @@ export default function AuctionDetails({params}: {params: {id: string}}) {
 
     setData(result)
     let max = 0
-    result.actions.map((item: any) => {
-      if (item.type === 'make_bid' && item.price > max) max = item.price
-    })
+    const actions = result.actions.sort((a: any, b: any) => new Date(a.updatedAt).getTime() > new Date(b.updatedAt).getTime() ? -1 : 1);
+
+    actions.map((item: any, index: number) => {
+        if (item.type === 'make_bid') {
+          if (!(actions[index - 1] && actions[index - 1].user._id === item.user._id && actions[index - 1].type === 'deny_bid')) {
+            if (item.price > max) max = item.price
+          }
+        }
+      })
     if (!bidPrice || bidPrice <= max) setBidPrice(Math.max(max, result.priceStarting) + result.priceStep)
     setCurTopPrice(max)
     setLoading(false)
@@ -39,10 +48,23 @@ export default function AuctionDetails({params}: {params: {id: string}}) {
       .filter((item: any) => item.user._id === user._id)
       .sort((a: any, b: any) => new Date(a.updatedAt).getTime() > new Date(b.updatedAt).getTime() ? -1 : 1)
     let max = 0
+    let hasDenyBid = false
+    let hasFinalizeBid = false
     userActions.map((item: any) => {
       if (item.type === 'make_bid' && item.price > max) max = item.price
+      if (item.type === 'deny_bid') {
+        hasDenyBid = true
+      }
+      if (item.type === 'finalize_bid') {
+        hasFinalizeBid = true
+      }
     })
+    setUserDenied(hasDenyBid)
+    setUserFinalizeBid(hasFinalizeBid)
     setCurUserTopPrice(max)
+    const now = Math.floor(new Date().getTime() / 1000)
+    const won = !hasDenyBid && max === curTopPrice && data.endTime < now
+    setUserWon(won)
   }
 
   const toggleBidPrice = (inc: boolean = true) => {
@@ -62,11 +84,23 @@ export default function AuctionDetails({params}: {params: {id: string}}) {
 
   const renderActionHistories = (histories: Array<any>) => {
     const tmp = histories.sort((a: any, b: any) => new Date(a.createdAt).getTime() > new Date(b.createdAt).getTime() ? -1 : 1)
-    return tmp.map((item: any, index: number) => <BidTeaser auctionId={data._id} order={index + 1} {...item} key={index}/>)
+    return tmp.map((item: any, index: number) => <BidTeaser
+      key={index}
+      auctionId={data._id}
+      startTime={data.startTime}
+      endTime={data.endTime}
+      order={index + 1}
+      {...item}
+      loading={loading}
+      handleDeny={handleDeny}
+    />)
   }
 
   const validDate = () => {
     const now = Math.floor(new Date().getTime() / 1000)
+    if (user && userDenied) {
+      return false
+    }
     return data.startTime <= now && data.endTime >= now
   }
 
@@ -97,6 +131,45 @@ export default function AuctionDetails({params}: {params: {id: string}}) {
     setLoading(false)
   }
 
+  const handlefinalizeBid = async () => {
+    setLoading(true)
+    const result = await post(`/auctions/finalize_bid`, {
+      auctionId: data._id,
+      userId: user._id
+    })
+    if (result.status) {
+      alert('Xác nhận thành công.')
+      await getAllAuctions()
+    }
+    else {
+      alert('Something wrong.')
+    }
+    setLoading(false)
+  }
+
+  const handleDeny = async () => {
+    setLoading(true)
+    const confirmed = confirm('Bạn có chắc chắn rút lại giá đã trả?')
+    if (!confirmed) {
+      setLoading(false)
+      return
+    }
+
+    const result = await post('/auctions/deny_bid', {
+      auctionId: data._id,
+      userId: user._id
+    })
+
+    if (result.status) {
+      alert('Success')
+      await getAllAuctions()
+    }
+    else {
+      alert('Something wrong')
+    }
+    setLoading(false)
+  }
+
   const renderAuctionTime = (data: any) => {
     const now = Math.floor(new Date().getTime() / 1000)
 
@@ -123,13 +196,93 @@ export default function AuctionDetails({params}: {params: {id: string}}) {
     </div>
   }
 
+  const renderBidAction = () => {
+    if (userWon) {
+      return <div className="flex flex-col gap-4">
+        <div className="text-green-500 text-center">
+          <strong>Chúc mừng. Bạn là người chiến thắng</strong>
+        </div>
+        {
+          userFinalizeBid ? ''
+            :
+            <div className="flex gap-4 items-center justify-center">
+              <button
+                onClick={handleDeny}
+                className="bg-red-500 text-white px-4 py-2 rounded"
+              >Huỷ kết quả</button>
+              <button
+                onClick={handlefinalizeBid}
+                className="bg-blue-500 text-white px-4 py-2 rounded"
+              >Nhận sản phẩm</button>
+            </div>
+        }
+      </div>
+    }
+
+    return <div className="flex justify-between items-center bid-action">
+      <div className="input-wrapper">
+        <div className="input">
+          <button
+            disabled={!validDate() || loading}
+            onClick={() => {
+              toggleBidPrice(false)
+            }}
+          >-
+          </button>
+          <span className="value">{bidPrice ? numberFormat(bidPrice) : 0}</span>
+          <button
+            disabled={!validDate() || loading}
+            onClick={() => {
+              toggleBidPrice(true)
+            }}
+          >+
+          </button>
+        </div>
+        <div>
+          <button
+            disabled={!validDate() || loading}
+            onClick={bidPriceToTop}
+          >Đặt giá cao hơn mức giá cao nhất 01 (một) bước giá
+          </button>
+        </div>
+      </div>
+      <div className="btns">
+        <button
+          disabled={!validDate() || loading}
+          onClick={handleAuction}
+        >
+          {
+            loading ?
+              <svg aria-hidden="true" className="w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
+                   viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path
+                  d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                  fill="currentColor"/>
+                <path
+                  d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                  fill="currentFill"/>
+              </svg>
+              :
+              <svg className="svg-inline--fa fa-gavel" aria-hidden="true" focusable="false" data-prefix="fas"
+                   data-icon="gavel" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"
+                   data-fa-i2svg="">
+                <path fill="currentColor"
+                      d="M318.6 9.4c-12.5-12.5-32.8-12.5-45.3 0l-120 120c-12.5 12.5-12.5 32.8 0 45.3l16 16c12.5 12.5 32.8 12.5 45.3 0l4-4L325.4 293.4l-4 4c-12.5 12.5-12.5 32.8 0 45.3l16 16c12.5 12.5 32.8 12.5 45.3 0l120-120c12.5-12.5 12.5-32.8 0-45.3l-16-16c-12.5-12.5-32.8-12.5-45.3 0l-4 4L330.6 74.6l4-4c12.5-12.5 12.5-32.8 0-45.3l-16-16zm-152 288c-12.5-12.5-32.8-12.5-45.3 0l-112 112c-12.5 12.5-12.5 32.8 0 45.3l48 48c12.5 12.5 32.8 12.5 45.3 0l112-112c12.5-12.5 12.5-32.8 0-45.3l-1.4-1.4L272 285.3 226.7 240 168 298.7l-1.4-1.4z"></path>
+              </svg>
+          }
+          <span>Trả giá</span>
+        </button>
+      </div>
+    </div>
+  }
+
   useEffect(() => {
     getData()
   }, [auctions])
 
   useEffect(() => {
     getUserData()
-  }, [data, user])
+  }, [data, user?._id])
 
   if (!data) return <div className="mt-8">
     <h1>All Auction</h1>
@@ -164,10 +317,6 @@ export default function AuctionDetails({params}: {params: {id: string}}) {
             <span>Bước giá</span>
             <strong className={'text-orange-400'}>{numberFormat(data.priceStep, true)}</strong>
           </div>
-          <div className={'info'}>
-            <span>Trạng thái</span>
-            <span>{data.status}</span>
-          </div>
         </div>
         <div className="actions box flex flex-col gap-4">
           <div className={'flex justify-between'}>
@@ -189,54 +338,12 @@ export default function AuctionDetails({params}: {params: {id: string}}) {
           <div className="flex justify-between">
             <span>Giá cao nhất của bạn:</span> <strong className="text-orange-500">{numberFormat(curUserTopPrice, true)}</strong>
           </div>
-          <div className="flex justify-between items-center bid-action">
-            <div className="input-wrapper">
-              <div className="input">
-                <button
-                  disabled={!validDate() || loading}
-                  onClick={() => {toggleBidPrice(false)}}
-                >-</button>
-                <span className="value">{bidPrice ? numberFormat(bidPrice) : 0}</span>
-                <button
-                  disabled={!validDate() || loading}
-                  onClick={() => {toggleBidPrice(true)}}
-                >+</button>
-              </div>
-              <div>
-                <button
-                  disabled={!validDate() || loading}
-                  onClick={bidPriceToTop}
-                >Đặt giá cao hơn mức giá cao nhất 01 (một) bước giá</button>
-              </div>
-            </div>
-            <div className="btns">
-              <button
-                disabled={!validDate() || loading}
-                onClick={handleAuction}
-              >
-                {
-                  loading ?
-                    <svg aria-hidden="true" className="w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
-                         viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
-                      <path
-                        d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
-                        fill="currentColor"/>
-                      <path
-                        d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
-                        fill="currentFill"/>
-                    </svg>
-                    :
-                    <svg className="svg-inline--fa fa-gavel" aria-hidden="true" focusable="false" data-prefix="fas"
-                         data-icon="gavel" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"
-                         data-fa-i2svg="">
-                      <path fill="currentColor"
-                            d="M318.6 9.4c-12.5-12.5-32.8-12.5-45.3 0l-120 120c-12.5 12.5-12.5 32.8 0 45.3l16 16c12.5 12.5 32.8 12.5 45.3 0l4-4L325.4 293.4l-4 4c-12.5 12.5-12.5 32.8 0 45.3l16 16c12.5 12.5 32.8 12.5 45.3 0l120-120c12.5-12.5 12.5-32.8 0-45.3l-16-16c-12.5-12.5-32.8-12.5-45.3 0l-4 4L330.6 74.6l4-4c12.5-12.5 12.5-32.8 0-45.3l-16-16zm-152 288c-12.5-12.5-32.8-12.5-45.3 0l-112 112c-12.5 12.5-12.5 32.8 0 45.3l48 48c12.5 12.5 32.8 12.5 45.3 0l112-112c12.5-12.5 12.5-32.8 0-45.3l-1.4-1.4L272 285.3 226.7 240 168 298.7l-1.4-1.4z"></path>
-                    </svg>
-                }
-                <span>Trả giá</span>
-              </button>
-            </div>
-          </div>
+          {
+            userDenied ?
+              <div className="text-center text-red-500">Bạn không có quyền đấu giá</div>
+              :
+              renderBidAction()
+          }
         </div>
       </div>
       <div className="mt-6 bid-histories box">
--- src/components/entity/bid-teaser.tsx
+++ src/components/entity/bid-teaser.tsx
@@ -1,8 +1,6 @@
 import {numberFormat} from '@/utils/helper';
 import moment from 'moment';
 import {useAuths} from '@/context/useAuths';
-import {useState} from 'react';
-import {post} from '@/utils/data';
 
 interface BidInterface {
   order: number
@@ -15,30 +13,13 @@ interface BidInterface {
   [key: string]: any
 }
 export default function BidTeaser(props: BidInterface) {
-  const {user, getAllAuctions} = useAuths()
-  const [loading, setLoading] = useState<boolean>(false)
-  const handleDeny = async () => {
-    setLoading(true)
-    const confirmed = confirm('Bạn có chắc chắn rút lại giá đã trả?')
-    if (!confirmed) {
-      setLoading(false)
-      return
-    }
+  const {user} = useAuths()
 
-    const result = await post('/auctions/deny_bid', {
-      auctionId: props.auctionId,
-      userId: user._id
-    })
-
-    if (result.status) {
-      alert('Success')
-      await getAllAuctions()
-    }
-    else {
-      alert('Something wrong')
-    }
-    setLoading(false)
+  const validDate = () => {
+    const now = Math.floor(new Date().getTime() / 1000)
+    return props.startTime <= now && props.endTime >= now
   }
+
   return <tr className={
     props.type === 'deny_bid' ?
       'line-through text-gray-400'
@@ -52,11 +33,11 @@ export default function BidTeaser(props: BidInterface) {
     <td>{moment(props.createdAt).format('DD/MM/YYYY - HH:mm:ss.SSSS')}</td>
     <td className={'text-center'}>
       {
-        props.order === 1 && user && props.user._id === user._id && props.type === 'make_bid' ?
+        validDate() && props.order === 1 && user && props.user._id === user._id && props.type === 'make_bid' ?
           <button
             className={'text-red-400'}
-            disabled={loading}
-            onClick={handleDeny}
+            disabled={props.loading}
+            onClick={props.handleDeny}
           >
             <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill={`rgb(248 113 113 / var(--tw-text-opacity))`} viewBox="0 0 256 256"><path d="M216,48H176V40a24,24,0,0,0-24-24H104A24,24,0,0,0,80,40v8H40a8,8,0,0,0,0,16h8V208a16,16,0,0,0,16,16H192a16,16,0,0,0,16-16V64h8a8,8,0,0,0,0-16ZM112,168a8,8,0,0,1-16,0V104a8,8,0,0,1,16,0Zm48,0a8,8,0,0,1-16,0V104a8,8,0,0,1,16,0Zm0-120H96V40a8,8,0,0,1,8-8h48a8,8,0,0,1,8,8Z"></path></svg>
           </button>
